122e6d7f15e7f90b6068632b92a907b20ed5cae9
Olga Glukhenko <olga.gluhenko.15@gmail.com>
2023-10-30 00:33:10 +0600

    js-hw-5

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+Создайте две функции, add и multiply; пусть каждая принимает
+по два аргумента. Функция add должна складывать аргументы
+и возвращать результат, а функция multiply — перемножать
+аргументы.
+С помощью только этих двух функций вычислите следующее
+несложное выражение: 36325 * 9824 + 777
+*/
+
+// function add (a, b) {
+//     return a + b;
+// }
+// function multiply (a, b){
+//     return a * b;
+// }
+// const result = add(multiply(36325, 9824), 777);
+//     console.log(result)
+// 
+
+/*
+Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
+*/
+// const square = (number) => {
+//     return number * number
+// }
+// const result = square(2);
+//     console.log(result)
+
+/*
+ Сделайте функцию, которая возвращает сумму двух чисел.
+*/
+
+// const sum = (a, b) => {
+//     return a + b;
+// }
+// const result = sum(5, 9);
+// console.log(result)
+
+/*
+ Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+*/
+
+// function number (a, b, c) {
+//     return (a - b) / c
+// }
+// const result = number(2, 4, 7);
+// console.log(result)
+
+/*
+ Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+*/
+// function dayWeek (number) {
+//     switch (number) {
+//         case 1:
+//             return "понедельник";
+//         case 2:
+//             return "вторник";
+//         case 3:
+//             return "среда";
+//         case 4:
+//             return "четверг";
+//         case 5:
+//             return "пятница";
+//         case 6:
+//             return "суббота";
+//         case 7:
+//             return "воскресенье";
+//         default:
+//             return "Неверный день недели";
+//     }
+    
+// }
+// console.log(dayWeek(5))
